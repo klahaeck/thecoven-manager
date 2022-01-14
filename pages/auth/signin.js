9c39920d6ab46f6c549570e3bbad817fe8aa6ae8
Kla Haeck <kla.haeck@gmail.com>
2022-01-14 13:08:09 -0600

    updates

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,9 +1,26 @@
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import { getCsrfToken } from 'next-auth/react';
 import { Row, Col, Form, Button } from 'react-bootstrap';
 import Layout from '../../layouts/main';
+import { useForm, Controller } from 'react-hook-form';
 
 const SignIn = ({ csrfToken }) => {
+  const { register, control, handleSubmit, formState: { errors } } = useForm();
+  const router = useRouter();
+
+  const onSubmit = data => {
+    fetch('/api/auth/signin/email', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+      .then(response => router.push(response.url))
+      .catch(error => console.error(error))
+  }
+
   return (
     <>
       <Head>
@@ -18,12 +35,27 @@ const SignIn = ({ csrfToken }) => {
         </Row>
         <Row className="justify-content-center">
           <Col sm="8" md="6" lg="5" xl="4" className="text-center">
-            <Form method="post" action="/api/auth/signin/email">
-              <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
-              <Form.Group className="mb-3">
+            <Form method="post" action="/api/auth/signin/email" onSubmit={handleSubmit(onSubmit)}>
+              <input defaultValue={csrfToken} type="hidden" {...register("csrfToken")} />
+              <Controller
+                name="email"
+                control={control}
+                // disabled={isSubmitting}
+                rules={{
+                  required: true,
+                  pattern : /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                }}
+                render={({ field }) => <Form.Group className="mb-3">
                 <Form.Label htmlFor="email" visuallyHidden={true}>Email address</Form.Label>
-                <Form.Control name="email" id="email" type="email" placeholder="Email" />
-              </Form.Group>
+                <Form.Control {...field} />
+                {errors.email?.type === 'required' && <Form.Text className="text-danger">
+                  Email is required
+                </Form.Text>}
+                {errors.email?.type === 'pattern' && <Form.Text className="text-danger">
+                  Please enter a valid email address
+                </Form.Text>}
+              </Form.Group>}
+              />              
               
               <Button variant="primary" type="submit">Sign in with Email</Button>
             </Form>
